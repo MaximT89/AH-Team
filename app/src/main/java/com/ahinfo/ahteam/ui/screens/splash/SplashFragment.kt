eb23596ffc91d231575976e013b93021c11843a1
Максим Толстобров <maximah1989@yandex.ru>
2022-07-12 12:55:36 +0300

    Refactoring

--- app/src/main/java/com/ahinfo/ahteam/ui/screens/splash/SplashFragment.kt
+++ app/src/main/java/com/ahinfo/ahteam/ui/screens/splash/SplashFragment.kt
@@ -1,7 +1,13 @@
 package com.ahinfo.ahteam.ui.screens.splash
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.annotation.SuppressLint
+import android.util.Property
+import android.view.View
 import androidx.fragment.app.viewModels
 import com.ahinfo.ahteam.core.bases.BaseFragment
+import com.ahinfo.ahteam.core.extension.playSingleSet
 import com.ahinfo.ahteam.core.navigation.Destinations
 import com.ahinfo.ahteam.databinding.FragmentSplashBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -10,7 +16,14 @@ import dagger.hilt.android.AndroidEntryPoint
 class SplashFragment : BaseFragment<FragmentSplashBinding, SplashViewModel>(FragmentSplashBinding::inflate) {
     override val viewModel: SplashViewModel by viewModels()
 
-    override fun initView() {
+    override fun initView() = with(binding){
+        animateLogo(logo, 3500, View.ROTATION_Y).playSingleSet {
+
+        }
+
+        animateLogo(imgBackInLogo, 3500, View.ROTATION).playSingleSet {
+            navigateTo(Destinations.SPLASH_TO_AUTH.id)
+        }
 
     }
 
@@ -18,5 +31,12 @@ class SplashFragment : BaseFragment<FragmentSplashBinding, SplashViewModel>(Frag
 
     }
 
-
+    @SuppressLint("Recycle")
+    private fun animateLogo(view: View, durationTime: Long, rotation: Property<View, Float>) : AnimatorSet{
+        val rotate = ObjectAnimator.ofFloat(view, rotation, 0f, 720f)
+        return AnimatorSet().apply {
+            duration = durationTime
+            play(rotate)
+        }
+    }
 }
