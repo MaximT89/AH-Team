8547e256b2c9b03e1b07cbd8c589aec888cbb959
Максим Толстобров <maximah1989@yandex.ru>
2022-07-20 10:48:45 +0300

    Create update task project feature bases

--- app/src/main/java/com/ahinfo/ahteam/ui/screens/parser/updateTaskProject/UpdateTaskProjectViewModel.kt
+++ app/src/main/java/com/ahinfo/ahteam/ui/screens/parser/updateTaskProject/UpdateTaskProjectViewModel.kt
@@ -1,14 +1,15 @@
 package com.ahinfo.ahteam.ui.screens.parser.updateTaskProject
 
+import com.ahinfo.ahteam.R
 import com.ahinfo.ahteam.core.bases.BaseViewModel
 import com.ahinfo.ahteam.core.common.ResourceProvider
+import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
+@HiltViewModel
 class UpdateTaskProjectViewModel @Inject constructor(
     private val resourceProvider: ResourceProvider
 ) : BaseViewModel() {
 
-    override fun title(): String {
-        TODO("Not yet implemented")
-    }
+    override fun title(): String = resourceProvider.string(R.string.update_task)
 }
