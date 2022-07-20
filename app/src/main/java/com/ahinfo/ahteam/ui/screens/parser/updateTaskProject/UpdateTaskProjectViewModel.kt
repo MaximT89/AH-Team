ad27b3e0d9fb00501b284df7d002c7d4cfe42882
Максим Толстобров <maximah1989@yandex.ru>
2022-07-20 10:27:30 +0300

    Create bases structure for UpdateTaskProject

---
+++ app/src/main/java/com/ahinfo/ahteam/ui/screens/parser/updateTaskProject/UpdateTaskProjectViewModel.kt
@@ -0,0 +1,14 @@
+package com.ahinfo.ahteam.ui.screens.parser.updateTaskProject
+
+import com.ahinfo.ahteam.core.bases.BaseViewModel
+import com.ahinfo.ahteam.core.common.ResourceProvider
+import javax.inject.Inject
+
+class UpdateTaskProjectViewModel @Inject constructor(
+    private val resourceProvider: ResourceProvider
+) : BaseViewModel() {
+
+    override fun title(): String {
+        TODO("Not yet implemented")
+    }
+}
