daeefaaee944a35f9c42e2a335a055ba772883aa
Максим Толстобров <maximah1989@yandex.ru>
2022-08-30 09:40:17 +0300

    Create GetCatalogCategoriesApi

--- app/src/main/java/com/ahinfo/ahteam/data/parser/catalogCategory/remote/api/GetCatalogCategoriesApi.kt
+++ app/src/main/java/com/ahinfo/ahteam/data/parser/catalogCategory/remote/api/GetCatalogCategoriesApi.kt
@@ -8,6 +8,8 @@ import retrofit2.http.POST
 
 interface GetCatalogCategoriesApi {
 
+    //sdfhaskdf
+
     @POST("/parsing/parsing/get.sections/")
     suspend fun getCatalogCategories(@Body request : RequestGetCatalogCategories) : Response<ResponseGetCatalogCategories>
 }
